ce20d30d21ba57e39fa785f2d58c39f36ccfc515
syntheticfeather <2391317090@qq.com>
2025-07-04 14:27:01 +0800

    add some task

---
+++ JAVA_STUDY/1__HELLO_WORLD__/L.java
@@ -0,0 +1,17 @@
+
+public class L {
+
+    public void test() {
+        System.out.println("Hello World");// 自带\n
+        System.out.print("Hello World\n");
+        System.out.printf("%d\n", 5);// 格式化输出
+        // 没有\n不会刷新缓冲区
+        // System.out.print("Hello World");
+    }
+
+    public static void main(String[] args) {
+        var ins = new L();
+        ins.test();
+    }
+
+}
---
+++ JAVA_STUDY/3__OPERATION__/L.java
@@ -0,0 +1,21 @@
+
+public class L {
+
+    public void test() {
+        int a = 2;
+        int b = 10;
+        System.err.println(a + b);
+        System.err.println(a - b);
+        System.err.println(a * b);        
+        // 不运行程序先想想除出来是多少呢？
+        System.err.println(a / b);
+        // 哈这不是次方
+        System.err.println(a ^ b); 
+
+    }
+
+    public static void main(String[] args) {
+        L l = new L();
+        l.test();
+    }
+}
