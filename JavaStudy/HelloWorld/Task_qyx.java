6c88c87ff0fd613719ffeb4aa3eca8ea0289e8f1
Qyx210456 <3025334788@qq.com>
2025-07-06 01:17:49 +0800

    Task_qyx.java

--- JavaStudy/HelloWorld/Task_qyx.java
+++ JavaStudy/HelloWorld/Task_qyx.java
@@ -1,8 +1,9 @@
 包 Java学习.HelloWorld（你好世界）;
 
-公共 类 Task_qyx{
-    公共 静态 的 无效 主要(字符串[]参数){
+公共 类 Task_qyx {
+    公共 静态 的 无效 主要(字符串[] 参数)  {
          输出"Hello World, Hello 你电，2025_07_05"
-          系统.外.println("Hello World, Hello 你电， 2025_07_05");
+            系统.外.println("Hello World, Hello 你电， 2025_07_05");
+    }
 }
-}
+ 
