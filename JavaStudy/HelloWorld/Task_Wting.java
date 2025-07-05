de62d5854fd9cb6e99a2db998f74f4eacbce7c8d
wting8952 <3223808952@qq.com>
2025-07-05 23:41:18 +0800

    第一周

---
+++ JavaStudy/HelloWorld/Task_Wting.java
@@ -0,0 +1,8 @@
+package JavaStudy.HelloWorld;
+
+public class Task_Wting {
+    public static void main(String[] args) {
+        // 输出 "Hello World, Hello UESTC, 2025_07_05" 
+        System.out.println("Hello World, Hello UESTC, 2025_07_05");
+    }
+}
