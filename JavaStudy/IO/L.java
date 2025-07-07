de784fec616833fa8eb4f7e52af9c926b2c9502f
syntheticfeather <2391317090@qq.com>
2025-07-07 17:07:37 +0800

    OI study | Add Task

--- JavaStudy/IO/L.java
+++ JavaStudy/IO/L.java
@@ -2,17 +2,22 @@ package JavaStudy.IO;
 
 import java.util.Scanner;
 
-
 public class L {
 
     public void test() {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.print("Enter a number: " + sc.nextInt() + "\n");
+            // System.out.print("Enter a number: " + sc.nextInt() + "\n");
             // String s = sc.nextLine();// 不跳空格
-            String s = sc.next();// 跳过空格
-            System.out.println("tt" + s);
-        }
-        catch (Exception e) {
+            // 不定数量的输入            
+            while (true)
+            {
+                String str = sc.nextLine().trim();
+                if (str.isEmpty()) {
+                    break; // 如果输入为空，则退出循环
+                } 
+                System.out.println(str);               
+            }
+        } catch (Exception e) {
             System.out.println("Invalid input. Please enter a valid number.");
         }
     }
--- TestTool.java
+++ TestTool.java
@@ -1,6 +1,10 @@
-import JavaStudy.clas.L;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Random;
+import java.util.Scanner;
+
 public class TestTool {
 
     Random ran = new Random();
@@ -33,17 +37,97 @@ public class TestTool {
         return arr;
     }
 
+    public static void main(String[] args) {
+    }
+}
+
+class Task {
 
     public static void main(String[] args) {
-        L l = new L();
-        l.test();
-        JavaStudy.DataType.L l2 = new JavaStudy.DataType.L();
-        HashMap<String, Integer> map = new HashMap<>()
-        {
-            {
-                put("two", 2 );
+        Task t = new Task();
+        t.input();
+        t.changeGrade();
+        t.encrypt();
+    }
+
+    final int MAX_SIZE = 2048;
+
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+        try (FileWriter fw = new FileWriter(".\\JavaStudy\\FileOI\\Input.txt")) {
+            while (true) {
+                String str = sc.nextLine();
+                if (str.isEmpty())
+                    break;
+                fw.write(str.replace(" ", "_") + "\n");
             }
-        };
-        map.put("one", 1);
+        } catch (Exception e) {
+        }
+        sc.close();
+    }
+
+    public void changeGrade() {
+        // try(BufferedReader br= new BufferedReader(
+        // new InputStreamReader(new FileInputStream("Input.txt"),
+        // StandardCharsets.UTF_8)))
+        try (BufferedReader br = new BufferedReader(
+                new FileReader(".\\JavaStudy\\FileOI\\Input.txt"))) {
+            // 拿出姓名栏
+            String[] name = new String[MAX_SIZE];
+            int size = 0;
+            String comLine;
+            while ((comLine = br.readLine()) != null) {
+                name[size++] = comLine.split("_")[1];
+            }
+            // 读成绩表姓名
+            String[][] grade = new String[MAX_SIZE][3];
+            int s = 0;
+            try (BufferedReader br2 = new BufferedReader(
+                    new FileReader(".\\JavaStudy\\FileOI\\grade.txt"))) {
+                String line;
+                while ((line = br2.readLine()) != null) {
+                    String[] larr = line.split("_");
+                    grade[s][0] = larr[0]; // 学号
+                    grade[s][1] = larr[1]; // 姓名
+                    grade[s][2] = larr[2]; // 总分
+                    s++;
+                }
+            }
+            // 修改数据
+            for (int i = 0; i < s; i++) {
+                for (int j = 0; j < size; j++) {
+                    if (name[j].equals(grade[i][1])) {
+                        int score = Integer.parseInt(grade[i][2]) + 3; // 成绩 + 3
+                        grade[i][2] = score + "*"; // 在总分后面加一个 "*"`
+                    }
+                }
+            }
+            try (FileWriter fw = new FileWriter(".\\JavaStudy\\FileOI\\grade.txt")) {
+                for (int i = 0; i < s; i++) {
+                    fw.write(grade[i][0] + "_" + grade[i][1] + "_" + grade[i][2] + "\n");
+                }
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    public void encrypt() {
+        try (BufferedReader br = new BufferedReader(new FileReader(".\\JavaStudy\\FileOI\\grade.txt"))) {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(".\\JavaStudy\\FileOI\\encrypted.txt"))) {
+                {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        StringBuilder sb = new StringBuilder(line);
+                        for (int i = 0; i < sb.length(); i++) {
+                            sb.setCharAt(i, (char) (sb.charAt(i) - 3));
+                        }
+                        bw.write(sb.toString());
+                        bw.newLine();
+                    }
+                }
+            } catch (Exception e) {
+            }
+        } catch (Exception e) {
+        }
     }
 }
