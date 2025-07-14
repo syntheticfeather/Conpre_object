3a80401a58fc1bcefd5d47609974197c1aef7cba
syntheticfeather <2391317090@qq.com>
2025-07-14 19:55:10 +0800

    日志更新

---
+++ JavaStudy/Erro/L.java
@@ -0,0 +1,15 @@
+package JavaStudy.Erro;
+
+public class L {
+    public void test() throws Exception {
+        {
+            
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new L().test();
+
+    }
+
+}
---
+++ JavaStudy/Reflect/Task.java
@@ -0,0 +1,12 @@
+package JavaStudy.Reflect;
+
+public class Task {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+ * 此专题不做要求
+ */
